72df7cc2568b65dd597444c9db2d5ae6e2e0edbb
Roman Voronkin <roman.voronkin@gmail.com>
2021-05-03 22:21:56 +0300

    Пройден шаг 3

--- cosmic_flight/__init__.py
+++ cosmic_flight/__init__.py
@@ -3,10 +3,30 @@
 
 import time
 
+import curses
+
 
 def draw(canvas):
     row, column = (5, 20)
     canvas.addstr(row, column, 'Hello, World!')
     canvas.border()
+    curses.curs_set(False)
     canvas.refresh()
-    time.sleep(1)
+
+    star_row, star_col = (8, 8)
+    while True:
+        canvas.addstr(star_row, star_col, '*', curses.A_DIM)
+        canvas.refresh()
+        time.sleep(2.0)
+
+        canvas.addstr(star_row, star_col, '*')
+        canvas.refresh()
+        time.sleep(0.3)
+
+        canvas.addstr(star_row, star_col, '*', curses.A_BOLD)
+        canvas.refresh()
+        time.sleep(0.5)
+
+        canvas.addstr(star_row, star_col, '*')
+        canvas.refresh()
+        time.sleep(0.3)
